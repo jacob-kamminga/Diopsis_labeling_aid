f33850f23ddd1c9d001880e25ed15cb4464cd8bf
Jacob Kamminga <jacob.kammingaa@gmail.com>
2024-04-18 20:09:11 +0200

    Add breadcrumb navigation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import pandas as pd
 import random
 
